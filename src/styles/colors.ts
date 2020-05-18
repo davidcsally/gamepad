c5500e85f67fb88458ca7c22b0f7f21959ab9d32
unknown <davidcsally@gmail.com>
2020-05-17 23:27:46 -0700

    Add some colors

---
+++ src/styles/colors.ts
@@ -0,0 +1,6 @@
+// color palette 
+export const primary = '#0b132b'
+export const secondary = '#1c2541'
+export const tertiary = '#3a506b'
+export const quad = '#8da9c4'
+export const white = '#ffffff' 
