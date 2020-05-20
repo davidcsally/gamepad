45ddd652b22014995d109cc12370dad7c9936da6
David Sally <davidcsally@gmail.com>
2020-05-20 00:35:44 -0700

    Add global stylesheet to all pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import 'border-box.css'
 import 'normalize.css'
+import '../global.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
