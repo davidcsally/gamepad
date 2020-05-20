877ce4162c6d933067a3d8ca4624699061b15921
David Sally <davidcsally@gmail.com>
2020-05-19 23:38:51 -0700

    Update withPadding HOC

--- src/styleHocs/withPadding.tsx
+++ src/styleHocs/withPadding.tsx
@@ -1,7 +1,30 @@
 import React from 'react'
 import styled from 'styled-components'
 
-export const withPadding = (WrappedComponent, padding = 0) =>
+interface Props {
+  padding?: string;
+  paddingTop?: string;
+  paddingBottom?: string;
+  paddingRight?: string;
+  paddingLeft?: string;
+}
+
+const exists = value => typeof value !== 'undefined' ? true : false
+
+export const withPadding = (
+  WrappedComponent,
+  {
+    padding,
+    paddingTop,
+    paddingBottom,
+    paddingLeft,
+    paddingRight,
+  }: Props
+) =>
   styled(WrappedComponent)`
-    padding: ${padding};
+    ${exists(padding) && `padding: ${padding};`}
+    ${exists(paddingTop) && `padding-top: ${paddingTop};`}
+    ${exists(paddingBottom) && `padding-bottom: ${paddingBottom};`}
+    ${exists(paddingLeft) && `padding-left: ${paddingLeft};`}
+    ${exists(paddingRight) && `padding-right: ${paddingRight};`}
   `
