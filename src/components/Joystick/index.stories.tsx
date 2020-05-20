8999af36aebc73bccad2a7730922c974dbf3554d
unknown <davidcsally@gmail.com>
2020-05-19 22:45:23 -0700

    Update Joystick component

--- src/components/Joystick/index.stories.tsx
+++ src/components/Joystick/index.stories.tsx
@@ -1,15 +1,29 @@
 import React from 'react'
 import { number } from "@storybook/addon-knobs";
+import styled from 'styled-components'
 
-import { Props, Joystick } from '.'
+import { Joystick } from '.'
 import { withGamepad } from '../../hocs/withGamepad';
+import { quad } from '../../styles/colors';
+import { withMargin } from '../../styleHocs/withMargin';
+
+const Background = styled.div`
+  height: 100vh;
+  width: 100vw;
+  background-color: ${quad};
+`
+
+export const Normal = () => <Joystick xAxis={number('top: ', 0)} yAxis={number('left: ', 0)} />
+
+const WrappedJoystick = withMargin(withGamepad(Joystick), { margin: '0 auto' })
+
+export const WithGamepad = () => (
+  <Background>
+    <WrappedJoystick />
+  </Background>
+)
 
 export default {
   component: Joystick,
   title: 'Joystick'
 }
-
-export const Normal = () => <Joystick xAxis={number('top: ', 0)} yAxis={number('left: ', 0)} />
-
-const WrappedJoystick = withGamepad(Joystick)
-export const WithGamepad = () => <WrappedJoystick />
