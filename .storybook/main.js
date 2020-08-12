b5a4f65bb082a4c054c961dccc13a6f74f09ce0c
David Sally <davidcsally@gmail.com>
2020-08-11 18:13:10 -0700

    Upgrade to storybook 6

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,6 @@
 module.exports = {
   stories: ['../**/*.stories.tsx'],
   addons: [
-    '@storybook/preset-typescript',
     '@storybook/addon-actions',
     '@storybook/addon-links',
     '@storybook/addon-knobs/register',
